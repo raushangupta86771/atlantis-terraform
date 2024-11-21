d7c6b98ca2d4d38b67223f7149f7cf2b0fa47600
Raushan Kumar <raushan.kumar@yulu.bike>
2024-11-21 16:01:46 +0530

    added multiple

--- main.tf
+++ main.tf
@@ -1,9 +1,22 @@
 provider "aws" {
-  region = "*********"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = "us-west-2"
 }
 
 data "aws_region" "current" {}
 
-output "aws_region" {
+output "default_aws_region" {
   value = data.aws_region.current.name
+}
+
+data "aws_region" "secondary_current" {
+  provider = aws.secondary
+}
+
+output "secondary_aws_region" {
+  value = data.aws_region.secondary_current.name
 }
