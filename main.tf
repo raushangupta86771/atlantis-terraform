320ad7c729d6af0ad884b41318ba347d98cbd782
Raushan Kumar <raushan.kumar@yulu.bike>
2024-11-21 17:22:24 +0530

    added multiple without more

--- main.tf
+++ main.tf
@@ -14,3 +14,19 @@ output "default_aws_region" {
 output "secondary_aws_region" {
   value = "us-west-2"
 }
+
+output "default_aws_provider" {
+  value = "aws"
+}
+
+output "secondary_aws_provider" {
+  value = "aws.secondary"
+}
+
+output "aws_region_count" {
+  value = length([ "us-east-1", "us-west-2" ])
+}
+
+output "regions" {
+  value = ["us-east-1", "us-west-2"]
+}
